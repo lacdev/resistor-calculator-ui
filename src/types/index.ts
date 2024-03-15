6cde44c7f3ef92d6780aa3203b06e6b0792c966f
lacdev <56662567+lacdev@users.noreply.github.com>
2024-03-15 01:20:51 -0600

    add missing types

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,22 @@ export type Tolerance = {
   color: string
   tolerance: string
 }
+
+export interface ColourOption {
+  readonly value: string
+  readonly label: string
+  readonly color: string
+  readonly isFixed?: boolean
+  readonly isDisabled?: boolean
+}
+
+export type BandOption = {
+  value: number
+  label: string
+  color: string
+}
+
+export type MultiplierOrToleranceOption = {
+  value: string
+  label: string
+}
