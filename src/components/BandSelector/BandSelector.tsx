caa42d54513c2ad2fd592298099f8ba6f926e3ca
lacdev <56662567+lacdev@users.noreply.github.com>
2024-03-15 12:30:57 -0600

    add styles update to BandSelector colors

--- src/components/BandSelector/BandSelector.tsx
+++ src/components/BandSelector/BandSelector.tsx
@@ -1,24 +1,83 @@
-import Select, { ActionMeta, SingleValue } from 'react-select'
+import Select, {
+  ActionMeta,
+  MultiValue,
+  SingleValue,
+  StylesConfig,
+} from 'react-select'
+import chroma from 'chroma-js'
 
-type Option = {
-  label: string
-  value: string
-  color?: string
-}
+import { ColourOption } from '../../types'
 
 type BandSelectorProps = {
   label: string
-  options: Option[]
+  options: ColourOption[]
   isDisabled?: boolean
-  onChange: (
-    newValue: SingleValue<Option>,
-    actionMeta: ActionMeta<Option>
-  ) => void | undefined
+  onChange:
+    | ((
+        newValue: SingleValue<ColourOption> | MultiValue<ColourOption>,
+        actionMeta: ActionMeta<ColourOption>
+      ) => void)
+    | undefined
   placeholder: string
 }
 
 const BandSelector = (props: BandSelectorProps) => {
   const { options, placeholder, label, isDisabled, onChange } = props
+
+  // This code comes from the library itself.
+  // https://react-select.com/styles
+
+  const dot = (color = 'transparent') => ({
+    alignItems: 'center',
+    display: 'flex',
+
+    ':before': {
+      backgroundColor: color,
+      borderRadius: 10,
+      content: '" "',
+      display: 'block',
+      marginRight: 8,
+      height: 10,
+      width: 10,
+    },
+  })
+
+  const colourStyles: StylesConfig<ColourOption> = {
+    control: (styles) => ({ ...styles, backgroundColor: 'white' }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+      const color = chroma(data.color)
+      return {
+        ...styles,
+        backgroundColor: isDisabled
+          ? undefined
+          : isSelected
+          ? data.color
+          : isFocused
+          ? color.alpha(0.1).css()
+          : undefined,
+        color: isDisabled
+          ? '#ccc'
+          : isSelected
+          ? chroma.contrast(color, 'white') > 2
+            ? 'white'
+            : 'black'
+          : data.color,
+        cursor: isDisabled ? 'not-allowed' : 'default',
+
+        ':active': {
+          ...styles[':active'],
+          backgroundColor: !isDisabled
+            ? isSelected
+              ? data.color
+              : color.alpha(0.3).css()
+            : undefined,
+        },
+      }
+    },
+    input: (styles) => ({ ...styles, ...dot() }),
+    placeholder: (styles) => ({ ...styles, ...dot('#fff') }),
+    singleValue: (styles, { data }) => ({ ...styles, ...dot(data.color) }),
+  }
 
   return (
     <div className="select-option-container">
@@ -28,6 +87,7 @@ const BandSelector = (props: BandSelectorProps) => {
         placeholder={placeholder}
         onChange={onChange}
         isDisabled={isDisabled}
+        styles={colourStyles}
       />
     </div>
   )
