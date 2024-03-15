e5b01373e0416d060f683a1b438d4a54eeb835a0
lacdev <56662567+lacdev@users.noreply.github.com>
2024-03-14 23:04:33 -0600

    add api paths

---
+++ src/constants/api.ts
@@ -0,0 +1,7 @@
+export const LOCAL_BACKEND_URL = 'http://localhost:8000'
+
+export const RESISTOR_VALUES_API_PATH = 'resistor-values'
+
+export const TOLERANCE_VALUES_API_PATH = 'tolerances'
+
+export const MULTIPLIER_VALUES_API_PATH = 'multipliers'
