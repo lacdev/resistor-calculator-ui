473306d7e00728e6a9f7bb1792fdf6b96d224747
lacdev <56662567+lacdev@users.noreply.github.com>
2024-03-15 12:29:00 -0600

    add update to tolerance and multiplier helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,11 @@
+import { DROPDOWN_COLORS_MAP } from '../constants/calculator'
 import { Multiplier, Tolerance } from '../types'
 
 export const mapMultiplierOptions = (multipliers: Multiplier[] = []) => {
   return multipliers.map((option) => ({
     value: option.color,
     label: `${option.color} (x${option.multiplier})`,
+    color: DROPDOWN_COLORS_MAP[option.color],
   }))
 }
 
@@ -11,5 +13,6 @@ export const mapToleranceOptions = (tolerances: Tolerance[] = []) => {
   return tolerances.map((option) => ({
     value: option.tolerance,
     label: `${option.color} (${option.tolerance})`,
+    color: DROPDOWN_COLORS_MAP[option.color],
   }))
 }
